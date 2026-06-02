a9c329624ae5c2a0bb4dc0c66019c85d8c5d8dec
Mikolaj Majewski <MrIronik99@gmail.com>
2026-06-02 17:10:05 +0200

    A little bit messy but working (in horizontal mode) driver

--- Lib/SSD1306_Driver/ssd1306_driver.h
+++ Lib/SSD1306_Driver/ssd1306_driver.h
@@ -22,11 +22,85 @@
 
 #define SSD1306_PAGE_SIZE 8
 
+/* Error Handle enums */
+
 typedef enum
 {
   SSD1306_INIT_OK,
   SSD1306_INIT_FAIL
 } ssd1306_error_t;
+
+typedef enum
+{
+  SSD1306_CLOCK_OK,
+  SSD1306_CLOCK_FAIL,
+} ssd1306_config_clock_error_t;
+
+typedef enum
+{
+  SSD1306_MULTIPLEXER_OK,
+  SSD1306_MULTIPLEXER_FAIL
+} ssd1306_config_multiplexer_error_t;
+
+typedef enum
+{
+  SSD1306_OFFSET_OK,
+  SSD1306_OFFSET_FAIL
+} ssd1306_config_offset_error_t;
+
+typedef enum
+{
+  SSD1306_CHARGEPUMP_OK,
+  SSD1306_CHARGEPUMP_FAIL
+} ssd1306_config_chargepump_error_t;
+
+typedef enum
+{
+  SSD1306_MEMORYMODE_OK,
+  SSD1306_MEMORYMODE_FAIL
+} ssd1306_config_mem_mode_error_t;
+
+typedef enum
+{
+  SSD1306_MAPPING_OK,
+  SSD1306_MAPPING_FAIL
+} ssd1306_config_mapping_error_t;
+
+typedef enum
+{
+  SSD1306_COM_SCAN_OK,
+  SSD1306_COM_SCAN_FAIL
+} ssd1306_config_com_scan_error_t;
+
+typedef enum
+{
+  SSD1306_COM_PINS_OK,
+  SSD1306_COM_PINS_FAIL
+} ssd1306_config_com_pins_error_t;
+
+typedef enum
+{
+  SSD1306_CONTRAST_OK,
+  SSD1306_CONTRAST_FAIL
+} ssd1306_config_contrast_error_t;
+
+typedef enum
+{
+  SSD1306_PRECHARGE_OK,
+  SSD1306_PRECHARGE_FAIL
+} ssd1306_config_precharge_error_t;
+
+typedef enum
+{
+  SSD1306_VOLTAGE_OK,
+  SSD1306_VOLTAGE_FAIL
+} ssd1306_config_voltage_error_t;
+
+typedef enum
+{
+  SSD1306_INVERSE_OK,
+  SSD1306_INVERSE_FAIL
+} ssd1306_config_inverse_error_t;
 
 typedef struct ssd1306_i2c_driver
 {
@@ -38,8 +112,10 @@ typedef struct ssd1306_i2c_driver
 } ssd1306_i2c_driver_t;
 
 ssd1306_error_t SSD1306_BasicInit(ssd1306_i2c_driver_t *driver);
+void SSD1306_Display_OnOff(ssd1306_i2c_driver_t *driver, uint8_t state);
 
 void SSD1306_Draw_Pixel(ssd1306_i2c_driver_t *driver, uint8_t xpos, uint8_t ypos);
 void SSD1306_Write_Display(ssd1306_i2c_driver_t *driver);
+void SSD1306_Clear_Display(ssd1306_i2c_driver_t *driver);
 
 #endif /* SSD1306_DRIVER_H_ */
